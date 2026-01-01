ee8bc0cf85d7c93a5e7b73af67297e4639bd62a6
Fuady <a.m.fuady@gmail.com>
2026-01-01 22:41:07 +0700

    fix_predict

--- predict.py
+++ predict.py
@@ -10,63 +10,105 @@ MODEL_PATH = "models/fraud_model.pkl"
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
 
-# -----------------------------
-# Initialize Flask app
-# -----------------------------
-app = Flask(__name__)
+
+data_1 = {
+        "age": 41,
+        "incident_severity": "Total Loss",
+        "total_claim_amount": 71600,
+        "insured_hobbies": "chess",
+        "policy_state": "OH",
+        "number_of_vehicles_involved": 1,
+        "property_damage": "YES",
+        "auto_model": "92x",
+        "insured_occupation": "craft-repair",
+        "vehicle_claim": 52080,
+        "bodily_injuries": 1,
+        "months_as_customer": 328,
+        "insured_relationship": "husband",
+        "injury_claim": 6510,
+        "insured_zip": 466132,
+        "witnesses": 2,
+        "capital-loss": 0,
+        "authorities_contacted": "Police",
+        "property_claim": 13020,
+        "capital-gains": 53300,
+        "incident_type": "Single Vehicle Collision",
+        "insured_education_level": "MD",
+        "collision_type": "Side Collision",
+        "umbrella_limit": 0,
+        "policy_number": 521585,
+        "policy_csl": "250/500",
+        "insured_sex": "MALE",
+        "auto_year": 2004,
+        "auto_make": "Saab",
+        "policy_annual_premium": 1406.91,
+        "police_report_available": "YES",
+        "incident_hour_of_the_day": 5,
+        "policy_deductable": 1000
+}
+
+X = pd.DataFrame([data_1])
+fraud_prob  = model.predict_proba(X)[0,1]
+
+print(f'Fraud Probability: {fraud_prob}')
+
+# # -----------------------------
+# # Initialize Flask app
+# # -----------------------------
+# app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
-def health_check():
-    return jsonify({
-        "status": "ok",
-        "message": "Insurance Fraud Detection API is running"
-    })
+# @app.route("/", methods=["GET"])
+# def health_check():
+#     return jsonify({
+#         "status": "ok",
+#         "message": "Insurance Fraud Detection API is running"
+#     })
 
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    """
-    Expected input:
-    {
-        "data": [
-            {
-                "age": 35,
-                "policy_state": "CA",
-                "incident_severity": "Total Loss",
-                ...
-            }
-        ]
-    }
-    """
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     """
+#     Expected input:
+#     {
+#         "data": [
+#             {
+#                 "age": 35,
+#                 "policy_state": "CA",
+#                 "incident_severity": "Total Loss",
+#                 ...
+#             }
+#         ]
+#     }
+#     """
 
-    try:
-        payload = request.get_json()
+#     try:
+#         payload = request.get_json()
 
-        if "data" not in payload:
-            return jsonify({"error": "Missing 'data' field"}), 400
+#         if "data" not in payload:
+#             return jsonify({"error": "Missing 'data' field"}), 400
 
-        df = pd.DataFrame(payload["data"])
+#         df = pd.DataFrame(payload["data"])
 
-        # Predictions
-        prediction = model.predict(df)
-        probability = model.predict_proba(df)[:, 1]
+#         # Predictions
+#         prediction = model.predict(df)
+#         probability = model.predict_proba(df)[:, 1]
 
-        response = {
-            "prediction": prediction.tolist(),
-            "fraud_probability": probability.tolist()
-        }
+#         response = {
+#             "prediction": prediction.tolist(),
+#             "fraud_probability": probability.tolist()
+#         }
 
-        return jsonify(response)
+#         return jsonify(response)
 
-    except Exception as e:
-        return jsonify({
-            "error": str(e)
-        }), 500
+#     except Exception as e:
+#         return jsonify({
+#             "error": str(e)
+#         }), 500
 
 
-# -----------------------------
-# Run server
-# -----------------------------
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# # -----------------------------
+# # Run server
+# # -----------------------------
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
